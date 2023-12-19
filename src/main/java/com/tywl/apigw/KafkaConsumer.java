1506834afe2fe576d530936adc7a351b663c9ace
allen <yuyalun.allen@gmail.com>
2023-12-19 15:13:39 +0800

    [feat]:订阅4个主题的消息。

--- src/main/java/com/tywl/apigw/KafkaConsumer.java
+++ src/main/java/com/tywl/apigw/KafkaConsumer.java
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Component;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -39,15 +37,30 @@ public class KafkaConsumer implements ConsumerSeekAware {
                 String message = JSON.parseObject(record.value().toString()).getString("message");
                 fileWriter.write(extractKeyFieldValues(message) + "\n");
             }
-            log.info("消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
+            log.info("south-nginx: 消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
         } catch (IOException ioe) {
-            log.error("消息写入文件失败。");
+            log.error("south-nginx: 消息写入文件失败。");
         }
         ack.acknowledge();
     }
 
-//    @KafkaListener(topics = {"north-app"}, containerFactory = "batchFactory", errorHandler="consumerAwareErrorHandler")
-    public void extractionConsumer(List<ConsumerRecord<?, ?>> consumerRecords, Acknowledgment ack) {
+    @KafkaListener(topics = {"north-nginx"}, containerFactory = "batchFactory", errorHandler="consumerAwareErrorHandler")
+    public void northNginxExtraction(List<ConsumerRecord<?, ?>> consumerRecords, Acknowledgment ack) {
+        long start = System.currentTimeMillis();
+        try (FileWriter fileWriter = new FileWriter("north-nginx-message.txt", true)) {
+            for (ConsumerRecord<?, ?> record: consumerRecords) {
+                String message = JSON.parseObject(record.value().toString()).getString("message");
+                fileWriter.write(extractKeyFieldValues(message) + "\n");
+            }
+            log.info("north-nginx: 消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
+        } catch (IOException ioe) {
+            log.error("north-nginx: 消息写入文件失败。");
+        }
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = {"north-app"}, containerFactory = "batchFactory", errorHandler="consumerAwareErrorHandler")
+    public void northAppExtraction(List<ConsumerRecord<?, ?>> consumerRecords, Acknowledgment ack) {
         long start = System.currentTimeMillis();
         try (FileWriter fileWriter = new FileWriter("north-app-message.txt", true)) {
             for (ConsumerRecord<?, ?> record: consumerRecords) {
@@ -56,23 +69,32 @@ public class KafkaConsumer implements ConsumerSeekAware {
                 if (type == null || !type.equals("billing")) {
                     continue;
                 }
-                String keyValues = message.getString("apiCode") + "||" +
-                        message.getString("apiName") + "||" +
-                        message.getString("appCode") + "||" +
-                        message.getString("appName") + "||" +
-                        message.getString("date") + "||" +
-                        message.getString("timeGwReq") + "||" +
-                        message.getString("timeGwRes") + "||" +
-                        message.getString("timeThirdReq") + "||" +
-                        message.getString("timeThirdRes") + "||" +
-                        message.getString("resCode") + "||" +
-                        message.getString("urlIn") + "||" +
-                        message.getString("urlOut");
+                String keyValues = extractKeyFieldValues(message);
                 fileWriter.write(keyValues + "\n");
             }
-            log.info("消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
+            log.info("north-app: 消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
         } catch (IOException ioe) {
-            log.error("消息写入文件失败。");
+            log.error("north-app: 消息写入文件失败。");
+        }
+        ack.acknowledge();
+    }
+
+    @KafkaListener(topics = {"south-app"}, containerFactory = "batchFactory", errorHandler="consumerAwareErrorHandler")
+    public void southAppExtraction(List<ConsumerRecord<?, ?>> consumerRecords, Acknowledgment ack) {
+        long start = System.currentTimeMillis();
+        try (FileWriter fileWriter = new FileWriter("south-app-message.txt", true)) {
+            for (ConsumerRecord<?, ?> record: consumerRecords) {
+                JSONObject message = JSON.parseObject(record.value().toString());
+                String type = message.getString("logType");
+                if (type == null || !type.equals("billing")) {
+                    continue;
+                }
+                String keyValues = extractKeyFieldValues(message);
+                fileWriter.write(keyValues + "\n");
+            }
+            log.info("south-app: 消息写入文件成功！获取数据{}条，耗时{}ms", consumerRecords.size(),System.currentTimeMillis() - start);
+        } catch (IOException ioe) {
+            log.error("south-app: 消息写入文件失败。");
         }
         ack.acknowledge();
     }
@@ -84,130 +106,30 @@ public class KafkaConsumer implements ConsumerSeekAware {
             fieldValuePairs.put(fieldNames[i], values[i]);
         }
         return fieldValuePairs.get("$remote_addr") + "||" +
-               fieldValuePairs.get("$upstream_addr") + "||" +
                fieldValuePairs.get("$time_local") + "||" +
+               fieldValuePairs.get("$upstream_addr") + "||" +
                fieldValuePairs.get("$request_time") + "||" +
-               fieldValuePairs.get("$request") + "||" +
                fieldValuePairs.get("$status") + "||" +
+               fieldValuePairs.get("$request") + "||" +
                fieldValuePairs.get("$http_AppKey");
     }
 
-    public void batchConsumer(List<ConsumerRecord<?, ?>> consumerRecords, Acknowledgment ack) {
-        long start = System.currentTimeMillis();
-        List<TywlApigwLog> tywlApigwLogList = new ArrayList<>();
-        List<TywlApigwLogAggregation> groupMapList  = new ArrayList<>();
-        for (ConsumerRecord<?, ?> consumerRecord : consumerRecords) {
-            //log.info("消费的每条数据为：{}", consumerRecord.value());
-            TywlApigwLog tywlApigwLog = new TywlApigwLog();
-            try {
-                tywlApigwLog =  JSON.parseObject(consumerRecord.value().toString(),TywlApigwLog.class);
-            }catch (Exception e){
-                continue;
-            }
-            if (tywlApigwLog!=null && tywlApigwLog.getApiCode()!=null) {
-                tywlApigwLogList.add(tywlApigwLog);
-            }
-        }
-        //log.info("本次消费的数据量：{}", tywlApigwLogList.size());
-        if (tywlApigwLogList!=null && tywlApigwLogList.size()>0) {
-            Map<String, List<TywlApigwLog>> mapListGroupByName = tywlApigwLogList.stream().collect(Collectors.groupingBy(p ->
-                    p.getAccount() + "#" + p.getGwErrorCode() + "#" + Arrays.toString(p.getTags()) + "#" + p.getApiCode() + "#" + p.getApiName() + "#" +
-                            p.getAppCode() + "#" + p.getAppName() + "#" + p.getResCode() + "#" + "null" + "#" + "null"
-                            + "#" + "null" + "#" + p.getUserName() + "#" + p.getUserId()
-                            + "#" + p.getTimeGwReq().substring(0, 13) + ":00:00"
-            ));
-            //log.info("分组后：{}"+JSON.toJSONString(mapListGroupByName));
-
-            // 对分组数据进行求和操作
-            mapListGroupByName.forEach((groupName, mapByNameList) -> {
-                //log.info("groupName:{}"+groupName);
-                TywlApigwLogAggregation al = new TywlApigwLogAggregation();
-                Long sumTimeGwDuration = mapByNameList.stream().mapToLong(map -> map.getTimeGwDuration()).sum();
-                Long sumTimeThirdDuration = mapByNameList.stream().mapToLong(map -> map.getTimeThirdDuration()).sum();
-                Long totalNum = mapByNameList.stream().mapToLong(map -> map.getTimeGwDuration()).count();
-                OptionalLong maxTimeGwDuration = mapByNameList.stream().mapToLong(map -> map.getTimeGwDuration()).max();
-                OptionalLong maxTimeThirdDuration = mapByNameList.stream().mapToLong(map -> map.getTimeThirdDuration()).max();
-                OptionalLong minTimeGwDuration = mapByNameList.stream().mapToLong(map -> map.getTimeGwDuration()).min();
-                OptionalLong minTimeThirdDuration = mapByNameList.stream().mapToLong(map -> map.getTimeThirdDuration()).min();
-                OptionalDouble avgTimeGwDuration = mapByNameList.stream().mapToLong(map -> map.getTimeGwDuration()).average();
-                OptionalDouble avgTimeThirdDuration = mapByNameList.stream().mapToLong(map -> map.getTimeThirdDuration()).average();
-
-                if (!groupName.split("#")[0].equals("null")) {
-                    al.setUserAccount(groupName.split("#")[0]);
-                }
-                if (!groupName.split("#")[1].equals("null")) {
-                    al.setGwErrorCode(groupName.split("#")[1]);
-                }
-
-                if (!groupName.split("#")[2].equals("null")) {
-                    al.setClusterTags(groupName.split("#")[2].replace("[", "").replace("]", "").split(","));
-                }
-
-                if (!groupName.split("#")[3].equals("null")) {
-                    al.setApiCode(groupName.split("#")[3]);
-                }
-                if (!groupName.split("#")[4].equals("null")) {
-                    al.setApiName(groupName.split("#")[4]);
-                }
-                if (!groupName.split("#")[5].equals("null")) {
-                    al.setAppCode(groupName.split("#")[5]);
-                }
-                if (!groupName.split("#")[6].equals("null")) {
-                    al.setAppName(groupName.split("#")[6]);
-                }
-                if (!groupName.split("#")[7].equals("null")) {
-                    Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
-                    if (pattern.matcher(groupName.split("#")[7]).matches()) {
-                        al.setResCode(Integer.parseInt(groupName.split("#")[7]));
-                    }
-                }
-                if (!groupName.split("#")[8].equals("null")) {
-                    al.setCatalog1Code(groupName.split("#")[8]);
-                }
-                if (!groupName.split("#")[9].equals("null")) {
-                    al.setCatalog1Name(groupName.split("#")[9]);
-                }
-                if (!groupName.split("#")[10].equals("null")) {
-                    al.setRemoteIp(groupName.split("#")[10]);
-                }
-                if (!groupName.split("#")[11].equals("null")) {
-                    al.setUserName(groupName.split("#")[11]);
-                }
-                if (!groupName.split("#")[12].equals("null")) {
-                    al.setUserId(groupName.split("#")[12]);
-                }
-                if (!groupName.split("#")[13].equals("null")) {
-                    al.setDealTime(groupName.split("#")[13]);
-                }
-
-                al.setTotalNum(totalNum);
-                al.setSumTimeGwDuration(sumTimeGwDuration);
-                al.setSumTimeThirdDuration(sumTimeThirdDuration);
-                al.setMaxTimeGwDuration(maxTimeGwDuration.getAsLong());
-                al.setMaxTimeThirdDuration(maxTimeThirdDuration.getAsLong());
-                al.setMinTimeGwDuration(minTimeGwDuration.getAsLong());
-                al.setMinTimeThirdDuration(minTimeThirdDuration.getAsLong());
-                al.setAvgTimeGwDuration(avgTimeGwDuration.getAsDouble());
-                al.setAvgTimeThirdDuration(avgTimeThirdDuration.getAsDouble());
-                groupMapList.add(al);
-
-            });
-
-            // TODO 这边先不要打开，可以自己写入本地文件，上面的逻辑也可以先不要
-//            if (groupMapList != null && groupMapList.size() > 0) {
-//                for (TywlApigwLogAggregation tywlApigwLogAggregation : groupMapList) {
-//                    kafkaTemplate.send("accout-aggregate", JSON.toJSONString(tywlApigwLogAggregation)).addCallback(success -> {
-//                        // 消息在分区内的offset
-//                        long offset = success.getRecordMetadata().offset();
-//                        //log.info("发送消息到kafka队列成功:{}, offset为:{}", JSON.toJSONString(tywlApigwLogAggregation), offset);
-//                    }, failure -> {
-//                        log.error("发送消息到kafka队列失败:{}, 报错信息为:{}", JSON.toJSONString(tywlApigwLogAggregation), failure.getMessage());
-//                    });
-//                }
-//            }
-        }
-        //手动提交
-        ack.acknowledge();
-        //log.info("收到kafka的数据，拉取数据量：{}，消费时间：{}ms", consumerRecords.size(), (System.currentTimeMillis() - start));
+    private String extractKeyFieldValues(JSONObject message) {
+        return message.getString("apiCode") + "||" +
+               message.getString("apiName") + "||" +
+               message.getString("appCode") + "||" +
+               message.getString("appName") + "||" +
+               message.getString("urlIn") + "||" +
+               message.getString("urlOut") + "||" +
+               message.getString("date") + "||" +
+               message.getString("timeGwReq") + "||" +
+               message.getString("timeGwRes") + "||" +
+               message.getString("timeThirdReq") + "||" +
+               message.getString("timeThirdRes") + "||" +
+               message.getString("timeThirdDuration") + "||" +
+               message.getString("timeGwDuration") + "||" +
+               message.getString("resCode") + "||" +
+               message.getString("userId") + "||" +
+               message.getString("account");
     }
 }
